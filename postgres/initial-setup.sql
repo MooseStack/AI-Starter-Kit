1fd74ee85156ca6cd6d50710c1d6c74dcaae524a
Mustafa Bell <63203400+MooseStack@users.noreply.github.com>
2025-07-18 15:39:30 -0500

    update n8n workflows, add postgres data

--- postgres/initial-setup.sql
+++ postgres/initial-setup.sql
@@ -3,11 +3,43 @@
 --## Command executed for initial run of this script (enables pgvector and creates the table):
 --## psql -h localhost -p 5432 -U postgres -d postgres -f postgres/initial-setup.sql 
 
+-- This enables PGVector
 CREATE EXTENSION vector;
 
+-- This creates the documents table with the columns: id, content, metadata, and embedding
 CREATE TABLE public.documents (
   id bigserial PRIMARY KEY,         -- Unique identifier
   content text,                     -- Textual content of the document
   metadata jsonb,                   -- Additional metadata (e.g., title, source)
   embedding vector                  -- Vector embeddings (for similarity search, etc.)
 );
+
+-- Create a function to search for documents
+create function match_documents (
+  query_embedding vector(512),
+  match_count int DEFAULT null,
+  filter jsonb DEFAULT '{}'
+) returns table (
+  id bigint,
+  content text,
+  metadata jsonb,
+  embedding jsonb,
+  similarity float
+)
+language plpgsql
+as $$
+#variable_conflict use_column
+begin
+  return query
+  select
+    id,
+    content,
+    metadata,
+    (embedding::text)::jsonb as embedding,
+    1 - (documents.embedding <=> query_embedding) as similarity
+  from documents
+  where metadata @> filter
+  order by documents.embedding <=> query_embedding
+  limit match_count;
+end;
+$$;
